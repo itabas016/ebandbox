a1a9573127ba789352cfec4ab4b24355cf4fb2eb
arthor_cui <arthor.cui@gmail.com>
2013-09-04 21:07:45 +0800

    change service for news and add script for news table index

--- FrameMobile/src/TYDNewsService/Job/TouTiaoJob.cs
+++ FrameMobile/src/TYDNewsService/Job/TouTiaoJob.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FrameMobile.Domain;
+using FrameMobile.Domain.Service;
 using Quartz;
+using StructureMap;
 
-namespace TYDNewsService.Job
+namespace TYDNewsService
 {
     public class TouTiaoJob : IJob
     {
-        public void Execute(JobExecutionContext context)
+        public IDataBaseService dataBaseService
         {
-            throw new NotImplementedException();
+            get
+            {
+                if (_dataBaseService == null)
+                {
+                    _dataBaseService = ObjectFactory.GetInstance<IDataBaseService>();
+                }
+                return _dataBaseService;
+            }
+        }
+        private IDataBaseService _dataBaseService;
+
+        public TouTiaoJob()
+        {
+            Bootstrapper.Start();
+        }
+        public void Execute(IJobExecutionContext context)
+        {
+            FetchTouTiaoService service = new FetchTouTiaoService(dataBaseService);
+
+            //service.Capture();
         }
     }
 }
