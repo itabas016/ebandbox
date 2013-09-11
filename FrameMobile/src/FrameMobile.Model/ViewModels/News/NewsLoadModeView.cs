1239022e2a0d03fbad95ec8887f4e29d13c8c2bf
arthor_cui <arthor.cui@gmail,com>
2013-09-11 22:23:08 +0800

    rename news load mode table by subsonic

--- FrameMobile/src/FrameMobile.Model/ViewModels/News/NewsLoadModeView.cs
+++ FrameMobile/src/FrameMobile.Model/ViewModels/News/NewsLoadModeView.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using FrameMobile.Core;
 using Newtonsoft.Json;
+using SubSonic.SqlGeneration.Schema;
 
 namespace FrameMobile.Model
 {
+    [Serializable]
+    [SubSonicTableNameOverride("newsloadmode")]
     public class NewsLoadModeView : NewsSourceView
     {
     }
