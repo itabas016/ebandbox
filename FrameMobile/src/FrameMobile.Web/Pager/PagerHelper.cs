bb7ace744146549f9a98a38aa532e3c6d2efffc6
arthor_cui <arthor.cui@gmail.com>
2013-09-22 17:02:53 +0800

    add subsonic page display

---
+++ FrameMobile/src/FrameMobile.Web/Pager/PagerHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SubSonic.Schema;
+
+namespace FrameMobile.Web
+{
+    public static class PagerHelper
+    {
+        #region Html Pager
+        public static MvcHtmlString Pager(this HtmlHelper helper, int TotalCount, int pageSize, int pageIndex, string actionName, string controllerName,
+            PagerOptions pagerOptions, string routeName, object routeValues, object htmlAttributes)
+        {
+            var totalPageCount = (int)Math.Ceiling(TotalCount / (double)pageSize);
+            var builder = new PagerBuilder
+                (
+                    helper,
+                    actionName,
+                    controllerName,
+                    totalPageCount,
+                    pageIndex,
+                    pagerOptions,
+                    routeName,
+                    new RouteValueDictionary(routeValues),
+                    new RouteValueDictionary(htmlAttributes)
+                );
+            return builder.RenderPager();
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, int TotalCount, int pageSize, int pageIndex, string actionName, string controllerName,
+            PagerOptions pagerOptions, string routeName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        {
+            var totalPageCount = (int)Math.Ceiling(TotalCount / (double)pageSize);
+            var builder = new PagerBuilder
+                (
+                    helper,
+                    actionName,
+                    controllerName,
+                    totalPageCount,
+                    pageIndex,
+                    pagerOptions,
+                    routeName,
+                    routeValues,
+                    htmlAttributes
+                );
+            return builder.RenderPager();
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, int TotalCount, int pageSize, int pageIndex, string actionName, string controllerName,
+            PagerOptions pagerOptions, string routeName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes, IDictionary<string, string> searchParamer)
+        {
+            var totalPageCount = (int)Math.Ceiling(TotalCount / (double)pageSize);
+            var builder = new PagerBuilder
+                (
+                    helper,
+                    actionName,
+                    controllerName,
+                    totalPageCount,
+                    pageIndex,
+                    pagerOptions,
+                    routeName,
+                    routeValues,
+                    htmlAttributes,
+                    searchParamer
+                );
+            return builder.RenderPager();
+        }
+
+        private static MvcHtmlString Pager(HtmlHelper helper, PagerOptions pagerOptions, IDictionary<string, object> htmlAttributes)
+        {
+            return new PagerBuilder(helper, null, pagerOptions, htmlAttributes).RenderPager();
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList)
+        {
+            if (pagedList == null)
+                return Pager(helper, (PagerOptions)null, null);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, null, null, null, null);
+        }
+
+        //todo add parameter Dictionary<string, string>
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, null);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, null, null, null);
+        }
+        //todo new -xing
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, IDictionary<string, string> searchParameter)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, null);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, null, null, null, searchParameter);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, object htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, new RouteValueDictionary(htmlAttributes));
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, null, null, htmlAttributes);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, IDictionary<string, object> htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, htmlAttributes);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, null, null, htmlAttributes);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, string routeName, object routeValues)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, null);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, routeName, routeValues, null);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, string routeName, RouteValueDictionary routeValues)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, null);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, routeName, routeValues, null);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, string routeName, object routeValues, object htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, new RouteValueDictionary(htmlAttributes));
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, routeName, routeValues, htmlAttributes);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, PagerOptions pagerOptions, string routeName,
+            RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, pagerOptions, htmlAttributes);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, pagerOptions, routeName, routeValues, htmlAttributes);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, string routeName, object routeValues, object htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, null, new RouteValueDictionary(htmlAttributes));
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, null, routeName, routeValues, htmlAttributes);
+        }
+
+        public static MvcHtmlString Pager(this HtmlHelper helper, IPagedList pagedList, string routeName, RouteValueDictionary routeValues,
+            IDictionary<string, object> htmlAttributes)
+        {
+            if (pagedList == null)
+                return Pager(helper, null, htmlAttributes);
+            return Pager(helper, pagedList.TotalCount, pagedList.PageSize, pagedList.PageIndex, null, null, null, routeName, routeValues, htmlAttributes);
+        }
+
+        #endregion
+    }
+}
