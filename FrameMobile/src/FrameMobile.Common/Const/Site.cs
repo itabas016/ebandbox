5c3a7e3e1005c4308c9df11f99c4b01a9116929b
arthor_cui <arthor.cui@gmail.com>
2013-09-22 15:46:51 +0800

    delete original account view

---
+++ FrameMobile/src/FrameMobile.Common/Const/Site.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameMobile.Common
+{
+    public class Site
+    {
+        public const string COMPANY_NAME = "上海天奕达电子科技有限公司";
+
+        public const string COPY_RIGHT = "2010-2013";
+
+        public const string ICP = "沪ICP备10202509号-2";
+
+        public const string ABOUT_US_URL = "http://tydtech.com/aboutus.html";
+
+        public const string CONTACT_US_URL = "http://tydtech.com/suggestion.asp";
+
+        public const string URL = "http://tydtech.com";
+
+        public const string COMPANY_NAME_EN = "tydtech";
+    }
+}
