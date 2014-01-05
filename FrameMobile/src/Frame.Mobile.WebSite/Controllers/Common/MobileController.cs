d51d7a060fc2a3e9204d915f3fb474525ada5e37
arthor_cui <arthor.cui@gmail.com>
2014-01-05 21:59:05 +0800

    add theme wallpaper layout view and adjust original project path

---
+++ FrameMobile/src/Frame.Mobile.WebSite/Controllers/Common/MobileController.cs
@@ -0,0 +1,21 @@
+﻿using FrameMobile.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frame.Mobile.WebSite.Controllers
+{
+    public class MobileController : MvcControllerBase
+    {
+        //
+        // GET: /Common/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
