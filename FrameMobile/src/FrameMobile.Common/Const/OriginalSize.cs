4206b9e391f57e5bf9e4fb9f5c33fc31aa57d431
arthor_cui <arthor.cui@gmail.com>
2014-03-18 23:00:53 +0800

    add wallpaper size original and thumbnail size

---
+++ FrameMobile/src/FrameMobile.Common/Const/OriginalSize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameMobile.Common
+{
+    public class OriginalSize
+    {
+        public const string FHD_SCREEN = "1080x1920";
+        public const string HD_SCREEN = "720x1280";
+        public const string QHD_SCREEN = "540x960";
+        public const string FWVGA_SCREEN = "480x854";
+        public const string WVGA_SCREEN = "480x800";
+        public const string FHD_WIDESCREEN = "2160x1920";
+        public const string HD_WIDESCREEN = "1440x1280";
+        public const string QHD_WIDESCREEN = "1080x960";
+        public const string FWVGA_WIDESCREEN = "960x854";
+        public const string WVGA_WIDESCREEN = "960x800";
+    }
+}
